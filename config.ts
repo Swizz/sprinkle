46705424cc989768fd38ec61790575e5fb1b0f50
Swizz <Swizz@users.noreply.github.com>
2026-02-16 16:28:05 +0100

    Add css and layout default transformers

--- config.ts
+++ config.ts
@@ -2,7 +2,9 @@ import lume from "lume/mod.ts"
 import sprinkle from "./sprinkle.ts"
 import esbuild from "lume/plugins/esbuild.ts"
 import inline from "lume/plugins/inline.ts"
-// import minify from "lume/plugins/minify_html.ts"
+import lightning from "lume/plugins/lightningcss.ts"
+import purge from "lume/plugins/purgecss.ts"
+import minify from "lume/plugins/minify_html.ts"
 
 const site = lume({
   dest: "./dist",
@@ -16,11 +18,21 @@ site.use(esbuild({
 
 site.use(inline())
 
-// site.use(minify({
-//   options: {
-//     minify_js: true,
-//   }
-// }))
+site.use(lightning())
+
+site.use(purge({
+  options: {
+    variables: true
+  }
+}))
+
+site.use(minify({
+  options: {
+    minify_js: true,
+  }
+}))
+
+site.data("layout", "./_layout.html")
 
 site.add("script.js")
 
